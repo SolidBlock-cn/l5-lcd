e43f59f6416cb2e97082f716b312cd537041ec54
SolidBlock <32444848+SolidBlock-cn@users.noreply.github.com>
2023-06-15 08:46:53 +0800

    修复了一些小问题

--- src/Composition.tsx
+++ src/Composition.tsx
@@ -4,13 +4,13 @@ import './styles.css';
 import { Img, staticFile, useCurrentFrame } from 'remotion';
 
 const zhnames = ['中医药大学', '白沙六路', '光霞', '张家湾', '烽火村', '八铺街', '复兴路', '彭刘杨', '司门口黄鹤楼', '昙华林武胜门', '积玉桥', '三层楼', '三角路', '徐家棚', '杨园铁四院', '余家头', '科普公园', '建设二路', '和平公园', '红钢城', '青宜居', '工人村', '武钢', '厂前', '武汉站东广场']
-const ennames = ['Hubei University of Chinese Medicine', 'Bai_sha 6th Road', 'Guang_xia', 'Zhang_jia_wan', 'Feng_huo_cun', 'Ba_pu_jie', 'Fu_xing Road', 'Peng_liu_yang', 'Simenkou & Yellow Crane Tower', 'Tan_hua_lin & Wu_sheng_men', 'Ji_yu_qiao', 'San_ceng_lou', "San_jiao Road", 'Xu_jia_peng', 'Yangyuan & Tiesiyuan', 'Yu_jia_tou', 'Ke_pu_Park', 'Jian_she 2nd Road', 'He_ping Park', 'Hong_gang_cheng', 'Qing_yi_ju', 'Gong_ren_cun', 'Wu_gang', 'Chang_qian', 'East Square of Wuhan Railway Station']
+const ennames = ['Hubei University of Chinese Medicine', 'Bai_sha 6th Road', 'Guang_xia', 'Zhang_jia_wan', 'Feng_huo_cun', 'Ba_pu_jie', 'Fu_xing Road', 'Peng_liu_yang', 'Simenkou & Yellow Crane Tower', 'Tan_hua_lin & Wu_sheng_men', 'Ji_yu_qiao', 'San_ceng_lou', "San_jiao Road", 'Xu_jia_peng', 'Yangyuan & Tiesiyuan', 'Yu_jia_tou', 'Ke_pu Park', 'Jian_she 2nd Road', 'He_ping Park', 'Hong_gang_cheng', 'Qing_yi_ju', 'Gong_ren_cun', 'Wu_gang', 'Chang_qian', 'East Square of Wuhan Railway Station']
 
 const zhTransfers: Record<string, string> = {
 	'复兴路': '可换乘4号线',
 	'积玉桥': '可换乘2号线',
 	'徐家棚': '可换乘7号线、8号线',
-	'武汉站东广场': '可出站换乘4号线'
+	'武汉站东广场': '可出站转乘4号线'
 }
 const enTransfers: Record<string, string> = {
 	'复兴路': 'transfer to Line 4',
@@ -74,7 +74,7 @@ export const MyComposition = () => {
 		summaryClass = 'summary-destination'
 	} else if (splitTransfer ? timeSince % 270 < 90 : timeSince % 180 < 90) {
 		const transfer = zhTransfers[zhname]
-		summaryContent = <>{isArrived ? zhname + "到了" : "下一站：" + zhname + (transfer ? "，" + transfer : '')} </>
+		summaryContent = <>{(isArrived ? zhname + "到了" : "下一站：" + zhname) + (transfer ? "，" + transfer : '')} </>
 		summaryClass = 'summary-zh'
 	} else if (splitTransfer) {
 		if (timeSince % 270 < 180) {
